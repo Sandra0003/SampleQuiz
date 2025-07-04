b1f2c1321ec99c1bb6397696be64e4d674b0751a
Sandra0003 <sandrapuramthottathil54@gmail.com>
2025-07-04 22:24:43 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function second(){
+    document.getElementById("question2").style.display = "flex"
+    document.getElementById("question1").style.display = "none"
+}
+function third(){
+    document.getElementById("question3").style.display = "flex"
+    document.getElementById("question2").style.display = "none"
+}
+
+function crrt(){
+    document.querySelector('.ans').style.backgroundColor = "green"
+}
